e60ee97c88ccd9b55563bbd33ca5bb7a80cbc52b
EufMuun <eufmuun.matvey@gmail.com>
2024-06-29 15:53:52 +0300

    Initial commit

---
+++ src/main/java/dev/flexteam/flexboost/FlexboostApplication.java
@@ -0,0 +1,13 @@
+package dev.flexteam.flexboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexboostApplication.class, args);
+	}
+
+}
