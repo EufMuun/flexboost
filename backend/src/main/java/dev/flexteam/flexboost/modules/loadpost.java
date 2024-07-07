f4d82aaf7524fb975fb35a99e47fc63af8188f4e
dewreeh <severussenap@gmail.com>
2024-07-08 00:25:31 +0300

    добавление поста (пока обрубок)

---
+++ backend/src/main/java/dev/flexteam/flexboost/modules/loadpost.java
@@ -0,0 +1,55 @@
+package dev.flexteam.flexboost.modules;
+
+import dev.flexteam.flexboost.ConnectToDB;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.MinioException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class loadpost {
+    private ConnectToDB connect = new ConnectToDB();
+    private Connection connection;
+    private MinioClient minioClient;
+    public void APILogin() throws SQLException {
+        connection = connect.getConnection();
+        this.minioClient = minioClient;
+    }
+
+    public Map<String, String> loadpost(MultipartFile file ){
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .endpoint("http://127.0.0.1:9000")
+                        .credentials("flexboost", "flexboost")
+                        .build();
+
+        Map<String, String> response = new HashMap<>();
+        try {
+            // Получаем имя файла
+            String fileName = "postImg_" + file.getOriginalFilename();
+
+            // Загружаем файл в MinIO
+            InputStream inputStream = file.getInputStream();
+
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket("content").object("my-objectname").stream(
+                                    inputStream, size, -1)
+                            .contentType("video/mp4")
+                            .build());
+
+            // Возвращаем успешный ответ
+            response.put("message", "File uploaded successfully: " + fileName);
+        } catch (IOException | MinioException e) {
+            e.printStackTrace();
+            response.put("message", "Error uploading file: " + e.getMessage());
+        }
+        return response;
+    }
+}
+
